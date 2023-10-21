4211bb112521905a0150690a4d1c011ce8e2ee90
Ignacia Sierra <ignaciasierra@MacBook-Air-de-Ignacia.local>
2023-10-21 18:09:24 -0300

    hh

--- pagina web 2/pagina web mejorada/etiquetas.js
+++ pagina web 2/pagina web mejorada/etiquetas.js
@@ -1,25 +1,24 @@
-function selectFilterGroup(group) {
-    // Mostrar todas las imágenes cuando se selecciona "Todos"
-    if (group === "todos") {
-        const imagenes = document.querySelectorAll(".imagen");
-        imagenes.forEach(function (imagen) {
-            imagen.style.display = "block";
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén todos los botones de filtro
+    const botonesFiltro = document.querySelectorAll(".filtro");
+
+    // Agrega un controlador de eventos a cada botón
+    botonesFiltro.forEach(function (boton) {
+        boton.addEventListener("click", function () {
+            // Obtiene la etiqueta del botón de filtro
+            const etiqueta = boton.getAttribute("data-tag");
+
+            // Muestra todas las imágenes con la etiqueta correspondiente y oculta las demás
+            const imagenes = document.querySelectorAll(".imagen");
+            imagenes.forEach(function (imagen) {
+                const etiquetasImagen = imagen.getAttribute("data-tags");
+                if (etiquetasImagen.includes(etiqueta)) {
+                    imagen.style.display = "block";
+                } else {
+                    imagen.style.display = "none";
+                }
+            });
         });
-        return;
-    }
-
-    // Ocultar todas las imágenes
-    const imagenes = document.querySelectorAll(".imagen");
-    imagenes.forEach(function (imagen) {
-        imagen.style.display = "none";
     });
-
-    // Mostrar solo las imágenes con la etiqueta seleccionada
-    const etiqueta = group; // La etiqueta es igual al ID del grupo
-    imagenes.forEach(function (imagen) {
-        const etiquetasImagen = imagen.getAttribute("data-tags");
-        if (etiquetasImagen.includes(etiqueta)) {
-            imagen.style.display = "block";
-        }
-    });
-}
+});
